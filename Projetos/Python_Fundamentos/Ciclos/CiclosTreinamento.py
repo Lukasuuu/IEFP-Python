7423c1bc1b2276f756b179913eb92047edc60da7
Lukasuuu <luc.gabriel.2011@gmail.com>
2026-01-12 21:45:13 +0000

    Adiciona CiclosTreinamento.py

---
+++ Projetos/Python_Fundamentos/Ciclos/CiclosTreinamento.py
@@ -0,0 +1,36 @@
+def ciclos():
+    # Exemplo de uso do loop for
+    print("Exemplo de loop for:")
+    for i in range(5):
+        print(f"contagem em uma sequencia de 0 a 4: {i}")
+
+    # Exemplo de uso do loop while
+    print("\nExemplo de loop while:")
+    count = 0
+    while count < 5:
+        print(f"Contagem {count}")
+        count += 1
+ciclos()
+
+print(' ')
+
+for letras in 'Lucas':
+    print(letras)
+    
+nomes = ['Lucas', 'Mariana', 'Carlos', 'Beatriz' , 'Fernanda' , 'Gabriel']
+
+print(' ')
+
+for i in nomes:
+   
+    print(i)
+    print('-'*5)
+    
+print(' ')
+
+
+numeros = [10,20,30,40,50]
+
+for num in numeros:
+    print(num)
+print(' ')
