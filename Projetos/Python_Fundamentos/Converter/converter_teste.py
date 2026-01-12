123b6f0ad521e8adcb7c8f89b370e93ec514a04f
Lukasuuu <luc.gabriel.2011@gmail.com>
2026-01-12 22:25:43 +0000

    Atualiza converter_teste.py

--- Projetos/Python_Fundamentos/Converter/converter_teste.py
+++ Projetos/Python_Fundamentos/Converter/converter_teste.py
@@ -3,10 +3,19 @@
 # print(converter.euro_dollar(100))
 # print(converter.dollar_euro(100))
 
-from converter import euro_dollar,dollar_euro
+
+#outra forma de importar funções específicas dos módulos monetario euro e dollar
+from converter_monetario import euro_dollar,dollar_euro
 
 print(euro_dollar(100))
 print(dollar_euro(100))
+
+#outra forma de importar o módulo todo
+import converter_monetario
+
+print(converter_monetario.euro_dollar(100))
+print(converter_monetario.dollar_euro(100))
+
 
 '''DESAFIOOO DENOVOO'''
 # Desafio 11
@@ -15,9 +24,16 @@ print(dollar_euro(100))
 #fahrenheit e vice-versa
 #Importar o módulo e experimentar converter 32ºC para fahrenheit.
 
-import converter_temperatura
+#importando funções específicas dos módulos celsius e fahrenheit
+from converter_temperatura import celsius, fahrenheit
 
 valor= float(input('Me de um valor numerico de temperatura:'))
+
+print(f'Em celsius:{celsius(valor)}')
+print(f'Em fahrenheit: {fahrenheit(valor)}')
+
+#outra forma de importar o módulo todo
+import converter_temperatura
 
 print(f'Em celsius:{converter_temperatura.celsius(valor)}')
 print(f'Em fahrenheit: {converter_temperatura.fahrenheit(valor)}')
